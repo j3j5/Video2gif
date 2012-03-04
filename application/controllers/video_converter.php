7e7f371e0dc713e4cb118c537044e5fb0da4e96d
Julio Foulquié <jfoulquie@gmail.com>
2012-03-04 11:09:11 +0100

    application/controllers: rename the class name and add a new function 	to download the video out of a youtube URL, extract the frames, 	convert the frames into a GIF animation and delete all the temporary files.

--- application/controllers/video_converter.php
+++ application/controllers/video_converter.php
@@ -1,16 +1,16 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Video_converter extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
-	 *	- or -  
+	 *	- or -
 	 * 		http://example.com/index.php/welcome/index
 	 *	- or -
-	 * Since this controller is set as the default controller in 
+	 * Since this controller is set as the default controller in
 	 * config/routes.php, it's displayed at http://example.com/
 	 *
 	 * So any other public methods not prefixed with an underscore will
@@ -20,6 +20,70 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$this->load->view('welcome_message');
+	}
+
+	public function convert()
+	{
+		$video_filename = "video.flv";
+		$image_filename = "image.gif";
+		// 		$this->load->library('youtubegrabber');
+		if(!$this->input->post('url'))
+		{
+			echo 'no url!';
+		}
+		else
+		{
+			$url = $this->input->post('url');
+		}
+
+		if(!$this->input->post('start'))
+		{
+			$start = "";
+		}
+		else
+		{
+			$start = "-ss " . $this->input->post('start') . " ";
+		}
+
+		if(!$this->input->post('duration'))
+		{
+			$duration = "";
+		}
+		else
+		{
+			$duration = "-t " . $this->input->post('duration') . " ";
+		}
+
+		if(!$this->input->post('quality'))
+		{
+			$quality = "";
+		}
+		elseif($this->input->post('quality') !== 'sameq')
+		{
+			$quality = "-s " . $this->input->post('quality') . " ";
+		}
+		else
+		{
+			$quality = "-" . $this->input->post('quality');
+		}
+
+		require_once(dirname(__FILE__) . '/../libraries/Youtube_grabber.php');
+		$youtubegrabber = new youtubegrabber($url, 'temp/' . $video_filename, 0);
+
+		$extract_frames = "ffmpeg -i \"temp/$video_filename\" -r 5 $quality $start $duration -f image2 temp/image-%3d.jpeg";
+		exec($extract_frames);
+
+		$convert2gif = "convert temp/*.jpeg images/$image_filename";
+		exec($convert2gif);
+
+		$delete_video = "rm temp/$video_filename";
+		exec($delete_video);
+
+		$delete_frames = "rm temp/*.jpeg";
+		exec($delete_frames);
+
+		$this->load->helper('url_helper');
+		redirect("/images/$image_filename");
 	}
 }
 
